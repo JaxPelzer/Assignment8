d27bb395dc022ec5ddd5be41dcc12abf8e9f51ad
JaxPelzer <er7321re@go.minnstate.edu>
2021-11-28 23:08:28 -0600

    main

---
+++ assignment8/lib/api.dart
@@ -0,0 +1,30 @@
+import "package:dio/dio.dart";
+
+import './models/courses.dart';
+
+const String localhost = "http://10.0.2.2:1200/";
+
+class CourseApi {
+  final _dio = Dio(BaseOptions(baseUrl: localhost));
+
+  Future<List> getCourses() async {
+    final response = await _dio.get('/sortCourses');
+
+    return response.data['courses'];
+  }
+
+  Future<List> getStudents() async {
+    final response = await _dio.get('/getAllStudents');
+
+    return response.data['students'];
+  }
+
+  Future editCourse(String id, String name) async {
+    final response = await _dio
+        .post('/editCourseByID', data: {'_id': id, 'courseInstructor': name});
+  }
+
+  Future deleteCourse(String id) async {
+    final response = await _dio.post('/deleteCourseById', data: {'_id': id});
+  }
+}
